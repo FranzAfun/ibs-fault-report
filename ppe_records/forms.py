0f0eac945f425c1e867ef7a59c306f2127f82b12
Francis Kekeli Afun <francisafunk@gmail.com>
2026-04-14 15:46:40 +0000

    feat: add PPE forms with inline formset support
    * Created PPEIssueRecordForm for employee details
    * Created PPEItemForm for PPE entries
    * Configured inline formset for multiple PPE items

--- ppe_records/forms.py
+++ ppe_records/forms.py
@@ -1,1 +1,35 @@
 from django import forms
+from django.forms import inlineformset_factory
+
+from .models import PPEIssueRecord, PPEItem
+
+
+class PPEIssueRecordForm(forms.ModelForm):
+	class Meta:
+		model = PPEIssueRecord
+		fields = ['employee_name', 'project', 'job_title']
+		widgets = {
+			'employee_name': forms.TextInput(attrs={'class': 'input-control'}),
+			'project': forms.TextInput(attrs={'class': 'input-control'}),
+			'job_title': forms.TextInput(attrs={'class': 'input-control'}),
+		}
+
+
+class PPEItemForm(forms.ModelForm):
+	class Meta:
+		model = PPEItem
+		fields = ['ppe_type', 'date_issued', 'employee_signature']
+		widgets = {
+			'ppe_type': forms.TextInput(attrs={'class': 'input-control'}),
+			'date_issued': forms.DateInput(attrs={'type': 'date', 'class': 'input-control'}),
+			'employee_signature': forms.TextInput(attrs={'class': 'input-control'}),
+		}
+
+
+PPEItemFormSet = inlineformset_factory(
+	PPEIssueRecord,
+	PPEItem,
+	form=PPEItemForm,
+	extra=3,
+	can_delete=True,
+)
